62a21304d8d885ab75c5bb80bae60279a3244a92
Felice Forby <feliceforby@gmail.com>
2019-03-13 23:19:01 +0000

    Automatically backed up by Learn

--- app/assets/javascripts/tictactoe.js
+++ app/assets/javascripts/tictactoe.js
@@ -3,7 +3,7 @@ const WINNING_COMBOS = [[0,1,2], [3,4,5], [6,7,8], [0,3,6],
                         [1,4,7], [2,5,8], [0,4,8], [2,4,6]];
 
 var turn = 0;
-currentGame = 0; // 0 = false in JS
+var currentGame = 0; // 0 = false in JS
 
 $(function() {
   // Get buttons ready on load
